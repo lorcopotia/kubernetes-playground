a1d24df568edfd5ea485b5d81e4caf6494ccfc5c
Duanel Garrido <lorcopotia@gmail.com>
2023-09-29 12:17:30 +0200

    Update: modificacion a las paginas htmls

--- Labs-docker/Lab2/node-express-server/index.js
+++ Labs-docker/Lab2/node-express-server/index.js
@@ -3,13 +3,46 @@ const app = express(),
       port = 3080;
 
 app.get('/', (req,res) => {
-    res.send('La aplicacion FUNCIONA !!!');
+    res.send(`
+        <h1>Bienvenido a mi Aplicación</h1>
+        <p>Estás experimentando una increíble aplicación.</p>
+        <table border="1">
+            <tr>
+                <th>Nombre</th>
+                <th>Edad</th>
+            </tr>
+            <tr>
+                <td>John Doe</td>
+                <td>30</td>
+            </tr>
+            <tr>
+                <td>Jane Smith</td>
+                <td>28</td>
+            </tr>
+        </table>
+    `);
 });
 
 app.get('/time', (req,res) => {
-    res.send(new Date());
+    const now = new Date();
+    res.send(`
+        <h1>La Hora Actual</h1>
+        <p>La hora actual es: ${now.toLocaleString()}</p>
+        <table border="1">
+            <tr>
+                <th>Día</th>
+                <th>Mes</th>
+                <th>Año</th>
+            </tr>
+            <tr>
+                <td>${now.getDate()}</td>
+                <td>${now.getMonth() + 1}</td>
+                <td>${now.getFullYear()}</td>
+            </tr>
+        </table>
+    `);
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on the port::${port}`);
+    console.log(`Servidor escuchando en el puerto ::${port}`);
 });
